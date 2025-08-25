0b082ffa0693fb3289bb954e850e721d0220854b
Declan-Dmello <declandmello2757@gmail.com>
2025-08-25 17:43:25 +0530

    fixed some minor issues
    Signed-off-by: Declan-Dmello <declandmello2757@gmail.com>

--- transformers_101/learning_101/Transformer_sentiment/dataset_file.py
+++ transformers_101/learning_101/Transformer_sentiment/dataset_file.py
@@ -1,8 +1,9 @@
 import torch
+from sympy.core.random import shuffle
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from datasets import load_dataset
-import nltk
+#import nltk
 from nltk.tokenize import word_tokenize
 from collections import Counter
 
@@ -25,23 +26,29 @@ word2idx = {'<pad>': 0, '<unk>': 1}
 for word,count in word_counts.items():
     if count>= 2:
         word2idx[word] = len(word2idx)
+        #basically adds the word to the dict if its not there already
+        #basically gives a unique index to each word based on the length of the dict
 
 
-def encode_text(text, max_length=512):
+def encode_text(text, max_length=None):
     words = word_tokenize(str(text).lower())
     words = words[:max_length]  # Truncate to max_length
     return [word2idx.get(word, word2idx['<unk>']) for word in words]
+#basically the top word_counts and word2idx are used to create a dict
+#and then using the mapping of the dict the encode text function is used
 #basically creates the token ids for each word and also handles unknown words
 
 
 #iterates thru the batch and then encodes it
-def collate_batch(batch, max_length=512):
+#basically converts all text to indices and then tensor and basically gets the labels
+
+def collate_batch(batch, max_length=None):
     labels, texts = [],[]
     for item in batch:
         text = item["text"]
         label = item["label"]
         labels.append(label)
-        token_ids = torch.tensor(encode_text(text), dtype=torch.long)
+        token_ids = torch.tensor(encode_text(text, max_length), dtype=torch.long)
         texts.append(token_ids)
 
     labels = torch.tensor(labels,dtype=torch.long)
@@ -53,8 +60,6 @@ def collate_batch(batch, max_length=512):
 
 #train_iter = dataset["train"]
 train_loader = DataLoader(train_data,
-                          collate_fn=lambda batch: collate_batch(batch, max_length=512)
+                          collate_fn=lambda batch: collate_batch(batch, 512)
                           , shuffle=True, batch_size=32)
-
-
 
